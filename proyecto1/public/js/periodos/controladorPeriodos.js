8c04b61588c5d8f68398ce62fd3830ceee8b8ed7
equirosa <equirosa@ucenfotec.ac.cr>
2018-07-11 19:06:43 -0600

    Unificacion de las dos ramas, ademas de agregar el nuevo menu y cambiar el html para alineralo con el estandar.

--- proyecto1/public/js/periodos/controladorPeriodos.js
+++ proyecto1/public/js/periodos/controladorPeriodos.js
@@ -167,20 +167,6 @@ function mostrarBusquedaPeriodos(){
     }
 };
 
-function abrirFuncion(evt, funcion) {
-    let i, tabcontent, tablinks;
-
-    tabcontent = document.getElementsByClassName("tabContent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(funcion).style.display = "block";
-    evt.currentTarget.className += " active";
-};
 
 function limpiarFormulario(){
     inputCodigo.value = '';
